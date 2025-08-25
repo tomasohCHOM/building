3dcf1079af162156956655f047831ba0e4710572
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-08-25 15:42:41 -0700

    start llvm frontend tutorial: complete lexer

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <string>
+
+// The lexer returns tokens [0-255] if it is an unknown character, otherwise one
+// of these for known things.
+enum Token {
+  tok_eof = -1,
+
+  tok_def = -2,
+  tok_extern = -3,
+
+  tok_identifier = -4,
+  tok_number = -5,
+};
+
+static std::string IdentifierStr; // Filled in if tok_identifier
+static double NumVal;             // Filled in if tok_number
+
+// gettok - Return the next token from standard input
+static int gettok() {
+  static int LastChar = ' ';
+
+  // Skip any whitespace.
+  while (std::isspace(LastChar))
+    LastChar = getchar();
+
+  if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+    IdentifierStr = LastChar;
+    while (isalnum((LastChar = getchar())))
+      IdentifierStr += LastChar;
+    if (IdentifierStr == "def")
+      return tok_def;
+    if (IdentifierStr == "extern")
+      return tok_extern;
+    return tok_identifier;
+  }
+
+  if (isdigit(LastChar) || LastChar == '.') { // Number: [0-9.]+
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+    NumVal = strtod(NumStr.c_str(), 0);
+    return tok_number;
+  }
+  if (LastChar == '#') {
+    // Comment until end of line
+    do
+      LastChar = getchar();
+    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+    if (LastChar != EOF)
+      return gettok();
+  }
+
+  if (LastChar == EOF)
+    return tok_eof;
+  
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return ThisChar;
+}
+
