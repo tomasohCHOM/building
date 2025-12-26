29b32381fe9b23738063ab8ec2ec3d07c4829fc3
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-12-26 13:04:15 -0800

    connecting the parser - chapter 4 done!

--- http/cmd/tcplistener/main.go
+++ http/cmd/tcplistener/main.go
@@ -1,69 +1,34 @@
 package main
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
-	"io"
+	"http/internal/request"
 	"log"
 	"net"
 )
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(lines)
-
-		curr := ""
-		for {
-			buffer := make([]byte, 8)
-			n, err := f.Read(buffer)
-			if err != nil {
-				if curr != "" {
-					lines <- curr
-					curr = ""
-					break
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				log.Fatal("oops")
-			}
-			buffer = buffer[:n]
-			if i := bytes.IndexByte(buffer, '\n'); i != -1 {
-				curr += string(buffer[:i])
-				lines <- curr
-				buffer = buffer[i+1:]
-				curr = ""
-			}
-			curr += string(buffer)
-		}
-	}()
-
-	return lines
-}
-
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
-		log.Fatal("oops")
+		log.Fatal("listener error:", err)
 	}
 	defer listener.Close()
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Fatal("oops")
+			log.Fatal("error accepting connection:", err)
 		}
 		fmt.Println("Connection has been accepted from", conn.RemoteAddr())
 
-		lines := getLinesChannel(conn)
-		for line := range lines {
-			fmt.Println(line)
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("error parsing request:", err)
 		}
 
-		fmt.Println("Connection has been closed!")
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %s\n", req.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", req.RequestLine.HttpVersion)
 	}
 }
