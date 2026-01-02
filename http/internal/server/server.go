d51499467e2b26a39380d3711795807e033f7ad1
Tomas Oh <tomasoh@csu.fullerton.edu>
2026-01-02 13:55:47 -0800

    7.1 & 7.2

--- http/internal/server/server.go
+++ http/internal/server/server.go
@@ -1,15 +1,56 @@
 package server
 
-import "net"
+import (
+	"fmt"
+	"net"
+	"sync/atomic"
+)
 
 type Server struct {
-	Port int
+	listener net.Listener
+	closed   atomic.Bool
 }
 
-func Serve(port int) (*Server, error)
+func Serve(port int) (*Server, error) {
+	server := &Server{}
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+	server.listener = listener
+	go server.listen()
+	return server, nil
+}
 
-func (s *Server) Close() error
+func (s *Server) Close() error {
+	err := s.listener.Close()
+	if err != nil {
+		return err
+	}
+	s.closed.Store(true)
+	return nil
+}
 
-func (s *Server) listen()
+func (s *Server) listen() {
+	for {
+		if s.closed.Load() {
+			break
+		}
+		conn, err := s.listener.Accept()
+		if err != nil {
+			continue
+		}
+		go s.handle(conn)
+	}
+}
 
-func (s *Server) handle(conn net.Conn)
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+	out := []byte(
+		"HTTP/1.1 200 OK\r\n" +
+			"Content-Type: text/plain\r\n" +
+			"\r\n" +
+			"Hello World!",
+	)
+	conn.Write(out)
+}
