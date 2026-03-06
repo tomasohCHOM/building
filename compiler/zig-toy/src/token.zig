930da722069a44e8f300859f91457aefa083c9f8
Tomas Oh <tomasoh@csu.fullerton.edu>
2026-03-06 14:28:19 -0600

    remove root.zig, add tokens, continue with lexer

---
+++ compiler/zig-toy/src/token.zig
@@ -0,0 +1,47 @@
+const std = @import("std");
+
+pub const TokenKind = enum {
+    identifier,
+    int_literal,
+
+    keyword_let,
+    keyword_fn,
+    keyword_return,
+    keyword_if,
+    keyword_else,
+    keyword_true,
+    keyword_false,
+
+    colon,
+    semicolon,
+    comma,
+
+    l_paren,
+    r_paren,
+    l_brace,
+    r_brace,
+
+    plus,
+    minus,
+    star,
+    slash,
+
+    equal,
+    equal_equal,
+    bang_equal,
+
+    less,
+    less_equal,
+    greater,
+    greater_equal,
+
+    and_and,
+    or_or,
+
+    eof,
+};
+
+pub const Token = struct {
+    kind: TokenKind,
+    lexeme: []const u8,
+};
